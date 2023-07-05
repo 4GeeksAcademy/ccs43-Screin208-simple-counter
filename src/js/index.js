a1205d5885e6514ebec777abc42b202ff26f766f
Screin208 <snay208@gmail.com>
2023-07-05 08:03:37 +0000

    reloj

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,50 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propTypes from "prop-types";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+const Counter = (props) => {
+  return (
+    <div className="container mt-4 rounded">
+      <div className="row">
+        <div className="col rounded">
+          <i className="fas fa-stopwatch"></i>
+        </div>
+        <div className="col rounded">0</div>
+        <div className="col rounded">0</div>
+        <div className="col rounded">{props.fourthDigit % 10}</div>
+        <div className="col rounded">{props.thirdDigit % 10}</div>
+        <div className="col rounded">{props.secondDigit % 10}</div>
+        <div className="col rounded">{props.firstDigit % 10}</div>
+      </div>
+    </div>
+  );
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+Counter.propTypes = {
+  firstDigit: propTypes.number,
+  secondDigit: propTypes.number,
+  thirdDigit: propTypes.number,
+  fourthDigit: propTypes.number,
+};
+
+let timer = 0;
+
+setInterval(function () {
+  const fourth = Math.floor(timer / 1000);
+  const third = Math.floor(timer / 100);
+  const second = Math.floor(timer / 10);
+  const first = Math.floor(timer / 1);
+  timer++,
+    //render your react application
+    ReactDOM.render(
+      <Counter
+        firstDigit={first}
+        secondDigit={second}
+        thirdDigit={third}
+        fourthDigit={fourth}
+      />,
+      document.querySelector("#app")
+    );
+}, 1000);
